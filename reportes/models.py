308e2a9a0ffa586d0f4e12ba13500e093f32948f
fernandosg <fsg_tmp@hotmail.com>
2018-04-14 21:44:19 -0500

    Creando nuevos atributos para el modelo MensajeOperacion.

--- reportes/models.py
+++ reportes/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class MensajeOperacion(models.Model):
     mensaje=models.TextField()
     estado=models.BooleanField(default=False)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
 
+    
     def __str__(self):
         return self.mensaje+" "+str(self.estado)
