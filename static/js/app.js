e3d86712787c56500f81ac8ea8e328e8a66658ad
fernandosg <fsg_tmp@hotmail.com>
2018-04-15 01:08:20 -0500

    Añadiendo jquery datepicker.

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+$( function() {
+    $( "input#id_fecha_instalacion" ).datepicker();
+    $( "input#id_fecha_pago" ).datepicker();
+    $( "input#id_fecha_instalacion" ).datepicker();
+    $( "input#id_fecha_cobro" ).datepicker();
+} );
