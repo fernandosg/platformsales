bf6aa347d763d2904bc8ea7787986da2b2897657
fernandosg <fsg_tmp@hotmail.com>
2018-04-16 00:02:38 -0500

    Cambios para mostrar mensaje por adeudo en el mes en curso.

--- main/views.py
+++ main/views.py
@@ -20,7 +20,8 @@ class IndexView(TemplateView):
             clientes_deudores=self.get_clientes_deudores()
             return {"template_for_user":template_for_user,"contratos":clientes_deudores}
         else:
-            template_for_user="main\index_cliente.html"
+            adeudo_mes=self.se_debe_el_mes(self.request.user.id)
+            return {"template_for_user":"main\index_cliente.html",adeudo_mes:adeudo_mes}
 
     def get_clientes_deudores(self):
         ahora=datetime.today()
@@ -30,3 +31,12 @@ class IndexView(TemplateView):
         inicio_fecha=datetime.today().replace(day=1)
         clientes_si_pagaron=PagoCliente.objects.values_list("cliente__id",flat=True).filter(fecha_pago__range=[str(inicio_fecha),str(fecha_final)])
         return Contrato.objects.exclude(cliente__id__in=clientes_si_pagaron).filter(estado_contrato=True)
+
+    def se_debe_el_mes(self,user_id):
+        ahora=datetime.today()
+        ultimo_dia=calendar.monthrange(ahora.year,ahora.month)[1]
+        fecha_final=datetime(ahora.year, ahora.month, ultimo_dia)  #year, month, day
+        inicio_fecha=datetime.today().replace(day=1)
+
+        cliente_id=Cliente.objects.get(user_id=user_id).id
+        return True#PagoCliente.objects.filter(fecha_pago__range=[str(inicio_fecha),str(fecha_final)],cliente_id=cliente_id).count()>0
